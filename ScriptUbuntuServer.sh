8741071f1d61d47b57f4990e3ecd3c69e86cd4d5
xPeenPapi <156064701+xPeenPapi@users.noreply.github.com>
2025-02-10 22:40:27 -0700

    Mejoras

--- ScriptUbuntuServer.sh
+++ ScriptUbuntuServer.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
 # Solicitar el nombre del dominio
-read -p "Ingrese el nombre del dominio: " $dominio
+read -p "Ingrese el nombre del dominio: " dominio
 
-# Solicitar la dirección IP y validarla con regex
+# Patrón regex para validar la dirección IP
+ipPattern='^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+
+# Solicitar la dirección IP y validarla
 while true; do
-    read -p "Ingrese la dirección IP: " $ip
-    if [[ $ip =~ "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$" ]]; then
-         break
+    read -p "Ingrese la dirección IP: " ip
+    if [[ $ip =~ $ipPattern ]]; then
+        break
     else
-        echo "Dirección IP no válida. Intente de nuevo."
+        echo "Dirección IP no válida. Intentalo de nuevo"
     fi
 done
 
-
-# Agregar la zona DNS a named.conf.local
+# Agregar la zona DNS al archivo named.conf.local
 echo "zone \"$dominio\" IN {
     type master;
     file \"/etc/bind/db.$dominio\";
 };" | sudo tee -a /etc/bind/named.conf.local
 
-# Crear el archivo de zona db.reprobados.com a partir de la plantilla db.local
+# Crear el archivo de zona db.$dominio a partir de una plantilla
 sudo cp /etc/bind/db.local /etc/bind/db.$dominio
 
-# Configurar el archivo de zona db.reprobados.com
+# Configurar el archivo de zona db.$dominio
 sudo bash -c "cat > /etc/bind/db.$dominio" <<EOF
 ;
 ; BIND data file for $dominio
@@ -44,4 +46,4 @@ EOF
 # Reiniciar el servicio BIND para aplicar los cambios
 sudo systemctl restart bind9
 
-echo "Configuración completada para el dominio $dominio con la IP $ip."
+echo "Configuracion completada para el dominio $dominio con la IP $ip."
